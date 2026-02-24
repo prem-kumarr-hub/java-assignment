e64103cad4d0548e7d517a5dd0f2ae3ebc72d5c2
prem-kumarr-hub <ravichandraravi506@gmail.com>
2026-02-24 18:27:34 +0530

    Initial commit

---
+++ src/Example1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Example1 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a Number:");
+        int num= sc.nextInt();
+        if(num>0){
+            System.out.println("The given number is positive");
+        }
+        else if(num<0){
+            System.out.println("The given number is negative");
+        }
+        else{
+            System.out.println("The given number is zero");
+        }
+    }
+}
