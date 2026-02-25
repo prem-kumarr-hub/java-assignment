fea7491817e8c93119d5b97f6dbe020351f1876e
prem-kumarr-hub <ravichandraravi506@gmail.com>
2026-02-25 20:34:04 +0530

    Added Main.java file

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+class Calculator {
+    int add(int a, int b) {
+        return a + b;
+    }
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("Sum of 2 numbers: " + calc.add(5, 10));
+        System.out.println("Sum of 3 numbers: " + calc.add(5, 10, 15));
+        Animal a;
+        a = new Dog();
+        a.sound();
+    }
+}
